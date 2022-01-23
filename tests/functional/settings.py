b45885343ee61b661186770ea204ab4147af6c33
Kirill Varchenko <kirill.varchenko@gmail.com>
2022-01-23 16:09:19 +0300

    Add use_test_data to settings

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -5,6 +5,7 @@ class TestSettings(BaseSettings):
     es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
     redis_host: str = Field('http://127.0.0.1:6379', env='REDIS_HOST')
     service_url: str = Field('http://127.0.0.1:8000', env='SERVICE_URL')
+    use_test_data: bool = Field(False, env='USE_TEST_DATA')
 
     class Config:
         env_file = '.env'
