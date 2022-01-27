a30c21571bfbd3cf3db0f33535729e3899e18fa0
Kirill Varchenko <kirill.varchenko@gmail.com>
2022-01-27 12:34:37 +0300

    Add parametrization to test film, add test cache

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,107 +1,33 @@
+import json
+
 import pytest
 
-FILMS_PATH = '/film'
+FILM_PATH = '/film'
 
-### Проверка валидации параметров в film/search ###
+VALIDATION_PARAMS = [
+    ({'page[size]': 0}, 422, 'ensure this value is greater than or equal to 1'),
+    ({'page[size]': -1}, 422, 'ensure this value is greater than or equal to 1'),
+    ({'page[size]': 'a'}, 422, 'value is not a valid integer'),
+    ({'page[number]': 0}, 422, 'ensure this value is greater than or equal to 1'),
+    ({'page[number]': -1}, 422, 'ensure this value is greater than or equal to 1'),
+    ({'page[number]': 'a'}, 422, 'value is not a valid integer')
+]
 
+### Проверка валидации параметров в film ###
 @pytest.mark.asyncio
-async def test_data_validation_search_empty(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {})
+@pytest.mark.parametrize('request_data,response_status,body_msg', VALIDATION_PARAMS)
+async def test_film_params_validation(make_get_request, request_data, response_status, body_msg):
+    response = await make_get_request(FILM_PATH, request_data)
 
-    assert response.status == 422
+    assert response.status == response_status
     assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'field required'
+    assert response.body['detail'][0]['msg'] == body_msg
 
-@pytest.mark.asyncio
-async def test_data_validation_search_some(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'somequery'})
-
-    assert response.status == 404
-    assert len(response.body) == 1
-    assert response.body == {'detail': 'films not found'}
-
-@pytest.mark.asyncio
-async def test_data_validation_search_film_ok(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars'})
-
-    assert response.status == 200
-    assert len(response.body) > 0
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_size_ok(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[size]': 10})
-
-    assert response.status == 200
-    assert len(response.body) == 10
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_size_zero(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[size]': 0})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'ensure this value is greater than or equal to 1'
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_size_negative(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[size]': -1})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'ensure this value is greater than or equal to 1'
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_size_not_numeric(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[size]': 'a'})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'value is not a valid integer'
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_number_ok(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[number]': 1})
-
-    assert response.status == 200
-    assert len(response.body) > 0
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_number_zero(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[number]': 0})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'ensure this value is greater than or equal to 1'
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_number_negative(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[number]': -1})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'ensure this value is greater than or equal to 1'
-
-
-@pytest.mark.asyncio
-async def test_data_validation_search_page_number_not_numeric(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/search', {'query': 'Star Wars',
-                                                               'page[number]': 'a'})
-
-    assert response.status == 422
-    assert len(response.body) == 1
-    assert response.body['detail'][0]['msg'] == 'value is not a valid integer'
 
 ### Проверка валидации параметров поиска фильма по айди ###
 @pytest.mark.asyncio
 async def test_search_for_film_invalid_id(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/test', {})
+    response = await make_get_request(f'{FILM_PATH}/test', {})
 
     assert response.status == 422
     assert len(response.body) == 1
@@ -110,7 +36,7 @@ async def test_search_for_film_invalid_id(make_get_request):
 ### Проверка поиска конкретного фильма ###
 @pytest.mark.asyncio
 async def test_search_for_film(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/e7e6d147-cc10-406c-a7a2-5e0be2231327', {})
+    response = await make_get_request(f'{FILM_PATH}/e7e6d147-cc10-406c-a7a2-5e0be2231327', {})
 
     assert response.status == 200
     assert response.body.get('title') == 'Shooting Star'
@@ -118,20 +44,32 @@ async def test_search_for_film(make_get_request):
 ### Вывести все фильмы ###
 @pytest.mark.asyncio
 async def test_list_films(make_get_request):
-    response = await make_get_request(f'{FILMS_PATH}/', {'page[size]': 50,
-                                                         'page[number]': 1})
+    response = await make_get_request(f'{FILM_PATH}/', {'page[size]': 50,
+                                                        'page[number]': 1})
 
     assert response.status == 200
     assert len(response.body) == 50
 
-### Поиск с учётом кэша в Redis ###
+@pytest.mark.asyncio
+async def test_film_cached(make_get_request, redis_client):
+    await redis_client.flushall()
 
-    # Создать индекс в Elasticsearch.
-    # Заполнить данные в Elasticsearch.
-    # Сбросить кеш в Redis через flushall.
-    # Послать запрос в сервис.
-    # Проверить полученный результат:
-    #     HTTP-код — 200,
-    #     вернулся один элемент,
-    #     тело ответа совпадает с ожидаемым.
-    # Удалить данные из индекса в Elasticsearch.
+    # check query not return anything
+    new_uuid = 'a5bd690c-121b-4e5e-a4fc-21c2dc868d4f'
+    response = await make_get_request(f'{FILM_PATH}/{new_uuid}', {})
+    assert response.status == 404
+
+    # add data to cache
+    new_cache_data= {'uuid': new_uuid, 'title': 'Test film', 'imdb_rating': 1.1,
+                     'description': 'Test film description', 'genre': [],
+                     'actors': [], 'writers': [], 'directors': []}
+    new_cache_json = json.dumps(new_cache_data)
+    await redis_client.set(new_uuid, new_cache_json)
+
+    # try again
+    response = await make_get_request(f'{FILM_PATH}/{new_uuid}', {})
+    assert response.status == 200
+    assert len(response.body) > 0
+    assert response.body == new_cache_data
+
+    await redis_client.delete(new_uuid)
