bff46157f829a4a06cfddcabc769b03e6a910230
Kirill Varchenko <kirill.varchenko@gmail.com>
2022-01-29 20:16:49 +0300

    Switch wait_for_ functions from endless loops to backoff

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,15 +1,48 @@
 import asyncio
 import os
+import sys
 
 from elasticsearch import AsyncElasticsearch
 
 
-async def wait_for_es(es: AsyncElasticsearch, sleep_sec: float = 5) -> None:
-    while not await es.ping():
-        await asyncio.sleep(sleep_sec)
+async def wait_for_es(es: AsyncElasticsearch,
+                      start_sleep_time: float = 0.1,
+                      factor: float = 2,
+                      border_sleep_time: float = 10,
+                      max_tries: int = 20
+                      ) -> int:
+    """Функция для ожидания готовности ES.
+    Parameters
+    ----------
+    es : AsyncElasticsearch
+        клиент ES
+    start_sleep_time : float, optional
+        начальное время повтора, by default 0.1
+    factor : float, optional
+        во сколько раз нужно увеличить время ожидания, by default 2
+    border_sleep_time : float, optional
+        граничное время ожидания, by default 10
+    max_tries : int, optional
+        Максимальное количество попыток, by default 20
+    """
+    t = start_sleep_time
+    n = 0
+    exit_code = 1 # failure
+    while n < max_tries:
+        n += 1
+        if await es.ping():
+            exit_code = 0 # success
+            break
+        else:
+            await asyncio.sleep(t)
+            t = t * factor if t < border_sleep_time else border_sleep_time
+
     await client.close()
+    return exit_code
 
 if __name__ == '__main__':
     es_host = os.environ.get('ELASTIC_HOST') or "127.0.0.1:9200"
     client = AsyncElasticsearch(hosts=es_host)
-    asyncio.run(wait_for_es(client))
+    exit_code = asyncio.run(wait_for_es(client))
+    # Выходим с кодом, чтобы прервать цепочку вызовов через && при неудачном подключении
+    sys.exit(exit_code)
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,16 +1,49 @@
 import asyncio
 import os
+import sys
 
 import aioredis
 
 
-async def wait_for_redis(host: str, sleep_sec: float = 5) -> None:
+async def wait_for_redis(host: str,
+                      start_sleep_time: float = 0.1,
+                      factor: float = 2,
+                      border_sleep_time: float = 10,
+                      max_tries: int = 20
+                      ) -> int:
+    """Функция для ожидания готовности Redis.
+    Parameters
+    ----------
+    host : str
+        путь к Redis
+    start_sleep_time : float, optional
+        начальное время повтора, by default 0.1
+    factor : float, optional
+        во сколько раз нужно увеличить время ожидания, by default 2
+    border_sleep_time : float, optional
+        граничное время ожидания, by default 10
+    max_tries : int, optional
+        Максимальное количество попыток, by default 20
+    """
     redis = await aioredis.create_redis(host)
-    while not await redis.ping():
-        await asyncio.sleep(sleep_sec)
+    t = start_sleep_time
+    n = 0
+    exit_code = 1 # failure
+    while n < max_tries:
+        n += 1
+        if await redis.ping():
+            exit_code = 0 # success
+            break
+        else:
+            await asyncio.sleep(t)
+            t = t * factor if t < border_sleep_time else border_sleep_time
+
     redis.close()
     await redis.wait_closed()
+    return exit_code
 
 if __name__ == '__main__':
     redis_host = "redis://" + (os.environ.get('REDIS_HOST') or "127.0.0.1:6379")
-    asyncio.run(wait_for_redis(host=redis_host))
+    exit_code = asyncio.run(wait_for_redis(host=redis_host))
+    # Выходим с кодом, чтобы прервать цепочку вызовов через && при неудачном подключении
+    sys.exit(exit_code)
